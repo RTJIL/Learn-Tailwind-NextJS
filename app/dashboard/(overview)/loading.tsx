73f72d417cb341b1f9ba78a77746f9bde5b28ea7
RTJIL <161304626+RTJIL@users.noreply.github.com>
2025-09-21 15:22:23 +0300

    chap 9: streaming slowest components

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,6 @@
+// --- IGNORE --- This file is used to show a loading state while the dashboard data is being fetched.
+import DashboardSkeleton from "@/app/ui/skeletons";
+
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
